9be9f85ae1517c8a87306ff26040f8a39f5a5161
MD. Bakibillah Rahat <46396330+bakibillahrahat@users.noreply.github.com>
2023-09-25 00:11:07 +0600

    fix problem

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,12 @@
-/* eslint-disable prettier/prettier */
 import {
   ForbiddenException,
   Injectable,
 } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
@@ -17,6 +16,7 @@ export class AuthService {
     private jwt: JwtService,
     private config: ConfigService,
   ) {}
+
   async signup(dto: AuthDto) {
     // generate the password hash
     const hash = await argon.hash(dto.password);
@@ -28,6 +28,7 @@ export class AuthService {
           hash,
         },
       });
+
       return this.signToken(user.id, user.email);
     } catch (error) {
       if (
@@ -45,18 +46,19 @@ export class AuthService {
   }
 
   async signin(dto: AuthDto) {
-    // find the user by emailI
+    // find the user by email
     const user =
       await this.prisma.user.findUnique({
         where: {
           email: dto.email,
         },
       });
-    // if user does not exist throw exeption
+    // if user does not exist throw exception
     if (!user)
       throw new ForbiddenException(
         'Credentials incorrect',
       );
+
     // compare password
     const pwMatches = await argon.verify(
       user.hash,
@@ -80,10 +82,13 @@ export class AuthService {
     };
     const secret = this.config.get('JWT_SECRET');
 
-    const token = await this.jwt.signAsync(payload, {
-      expiresIn: '15m',
-      secret: secret,
-    });
+    const token = await this.jwt.signAsync(
+      payload,
+      {
+        expiresIn: '15m',
+        secret: secret,
+      },
+    );
 
     return {
       access_token: token,
