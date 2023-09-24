1ffb26a0efd5926602eb7f11c87efc257d882efe
MD. Bakibillah Rahat <46396330+bakibillahrahat@users.noreply.github.com>
2023-09-24 21:30:52 +0600

    fix error

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -74,7 +74,7 @@ export class AuthService {
     return this.signToken(user.id, user.email);
   }
 
-  async signToken(
+  signToken(
     userId: number,
     email: string,
   ): Promise<string> {
