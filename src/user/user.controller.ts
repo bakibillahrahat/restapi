c134445c83324f32b4e565bad96a0bc8b076bb48
MD. Bakibillah Rahat <46396330+bakibillahrahat@users.noreply.github.com>
2023-09-29 19:43:00 +0600

    add guards

---
+++ src/user/user.controller.ts
@@ -0,0 +1,14 @@
+import {
+  Controller,
+  Get,
+  UseGuards,
+} from '@nestjs/common';
+
+@Controller('users')
+export class UserController {
+  @UseGuards()
+  @Get('me')
+  getMe() {
+    return 'user info';
+  }
+}
