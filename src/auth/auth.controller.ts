8b61165e894d29bed6607dcf936a499e3fdc0f16
MD. Bakibillah Rahat <46396330+bakibillahrahat@users.noreply.github.com>
2023-09-29 23:28:40 +0600

    signin status code 200

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,12 +12,13 @@ import { AuthDto } from './dto';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
-  @HttpCode(HttpStatus.OK)
+
   @Post('signup')
   signUp(@Body() dto: AuthDto) {
     return this.authService.signUp(dto);
   }
 
+  @HttpCode(HttpStatus.OK)
   @Post('signin')
   signIn(@Body() dto: AuthDto) {
     return this.authService.signIn(dto);
