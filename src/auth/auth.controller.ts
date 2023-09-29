aea6c56cc72f686c3513cdb7a29a1ac558d39016
MD. Bakibillah Rahat <46396330+bakibillahrahat@users.noreply.github.com>
2023-09-29 23:26:25 +0600

    http code add.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,8 @@
 import {
   Body,
   Controller,
+  HttpCode,
+  HttpStatus,
   Post,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
@@ -10,7 +12,7 @@ import { AuthDto } from './dto';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
-
+  @HttpCode(HttpStatus.OK)
   @Post('signup')
   signUp(@Body() dto: AuthDto) {
     return this.authService.signUp(dto);
