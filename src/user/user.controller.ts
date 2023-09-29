b1707d27909f872e7ce46694a36b65fcf7fa1953
MD. Bakibillah Rahat <46396330+bakibillahrahat@users.noreply.github.com>
2023-09-29 23:17:20 +0600

    add decorator

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,18 +1,20 @@
 import {
   Controller,
   Get,
-  Req,
+  Patch,
   UseGuards,
 } from '@nestjs/common';
+import { User } from '@prisma/client';
 
-import { Request } from 'express';
 import { JwtGuard } from 'src/auth/guard';
-
+import { GetUser } from 'src/decorator';
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-  @UseGuards(JwtGuard)
   @Get('me')
-  getMe(@Req() req: Request) {
-    return req.user;
+  getMe(@GetUser() user: User) {
+    return user;
   }
+  @Patch()
+  editUser() {}
 }
