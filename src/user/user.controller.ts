91e56870a6ecb6d26a68f443054cf0f4dae65f50
MD. Bakibillah Rahat <46396330+bakibillahrahat@users.noreply.github.com>
2023-09-30 12:55:03 +0600

    fix some path after unit test

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,8 +6,8 @@ import {
 } from '@nestjs/common';
 import { User } from '@prisma/client';
 
-import { JwtGuard } from 'src/auth/guard';
-import { GetUser } from 'src/decorator';
+import { JwtGuard } from '../../src/auth/guard';
+import { GetUser } from '../../src/decorator';
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
