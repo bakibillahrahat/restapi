9562823844911e071336839473b8e6610d1f3e90
bakibillahrahat <bakibillahrahat@protonmail.com>
2023-11-08 23:52:05 +0600

    add prisma in user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-    async editUser(userId: number){}
+  constructor(private prisma: PrismaService) {}
+  async editUser(userId: number) {}
 }
