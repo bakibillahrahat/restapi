c8fc29197ed66aa58f80d519777784684fa7863f
bakibillahrahat <bakibillahrahat@protonmail.com>
2023-11-08 23:48:45 +0600

    add expectstatus

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -148,6 +148,7 @@ describe('App e2e', () => {
         .withHeaders({
           Authorization : 'Rahat $S{userAt}'
         })
+        .expectStatus(201)
       })
     });
     describe('Get bookmark by id', () => {});
